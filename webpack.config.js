316f472116dd4051b56444b0a813174c4d2134ed
Oded Welgreen <oded.welgreen@gmail.com>
2018-09-20 22:22:53 +0300

    POC

--- webpack.config.js
+++ webpack.config.js
@@ -5,67 +5,67 @@ const nodeEnv = process.env.NODE_ENV || 'development';
 const isProd = nodeEnv === 'production';
 
 const plugins = [
-  new webpack.DefinePlugin({
-    'process.env': {
-      NODE_ENV: JSON.stringify(nodeEnv)
-    }
-  }),
-  new HtmlWebpackPlugin({
-    title: 'Typescript Webpack Starter',
-    template: '!!ejs-loader!src/index.html'
-  }),
-  new webpack.LoaderOptionsPlugin({
-    options: {
-      tslint: {
-        emitErrors: true,
-        failOnHint: true
-      }
-    }
-  })
+    new webpack.DefinePlugin({
+        'process.env': {
+            NODE_ENV: JSON.stringify(nodeEnv)
+        }
+    }),
+    new HtmlWebpackPlugin({
+        title: 'MIDI Light Show',
+        template: '!!ejs-loader!src/index.html'
+    }),
+    new webpack.LoaderOptionsPlugin({
+        options: {
+            tslint: {
+                emitErrors: true,
+                failOnHint: true
+            }
+        }
+    })
 ];
 
 var config = {
-  devtool: isProd ? 'hidden-source-map' : 'source-map',
-  context: path.resolve('./src'),
-  entry: {
-    app: './index.ts'
-  },
-  output: {
-    path: path.resolve('./dist'),
-    filename: '[name].bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        enforce: 'pre',
-        test: /\.ts?$/,
-        exclude: ['node_modules'],
-        use: ['awesome-typescript-loader', 'source-map-loader']
-      },
-      !isProd
-        ? {
-            test: /\.(js|ts)$/,
-            loader: 'istanbul-instrumenter-loader',
-            exclude: [/\/node_modules\//],
-            query: {
-              esModules: true
-            }
-          }
-        : null,
-      { test: /\.html$/, loader: 'html-loader' },
-      { test: /\.css$/, loaders: ['style-loader', 'css-loader'] }
-    ].filter(Boolean)
-  },
-  resolve: {
-    extensions: ['.ts', '.js']
-  },
-  plugins: plugins,
-  devServer: {
-    contentBase: path.join(__dirname, 'dist/'),
-    compress: true,
-    port: 3000,
-    hot: true
-  }
+    devtool: isProd ? 'hidden-source-map' : 'source-map',
+    context: path.resolve('./src'),
+    entry: {
+        app: './index.ts'
+    },
+    output: {
+        path: path.resolve('./dist'),
+        filename: '[name].bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                enforce: 'pre',
+                test: /\.ts?$/,
+                exclude: [ 'node_modules' ],
+                use: [ 'awesome-typescript-loader', 'source-map-loader' ]
+            },
+            !isProd
+                ? {
+                      test: /\.(js|ts)$/,
+                      loader: 'istanbul-instrumenter-loader',
+                      exclude: [ /\/node_modules\// ],
+                      query: {
+                          esModules: true
+                      }
+                  }
+                : null,
+            { test: /\.html$/, loader: 'html-loader' },
+            { test: /\.css$/, loaders: [ 'style-loader', 'css-loader' ] }
+        ].filter(Boolean)
+    },
+    resolve: {
+        extensions: [ '.ts', '.js' ]
+    },
+    plugins: plugins,
+    devServer: {
+        contentBase: path.join(__dirname, 'dist/'),
+        compress: true,
+        port: 3000,
+        hot: true
+    }
 };
 
 module.exports = config;
